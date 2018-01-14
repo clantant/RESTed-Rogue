9f6ecabc3406ee8c624ec843142c65b2adf4177d
Eric Timmerman <eet1992@gmail.com>
2018-01-13 20:00:22 -0500

    Added build framework
    Put together the setup.py and fixed the directory structure for the game.

---
+++ setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+setup(name='Restgame',
+	version='0.1',
+	author="Eric Timmerman",
+	author_email = "eet1992@gmail.com",
+	description = ("REST based web game"),
+	packages=['restgame', 'test']
+	)
---
+++ test/test_main.py
@@ -0,0 +1,12 @@
+import unittest
+
+class DungeonValid(unittest.TestCase):
+
+	def testCorrectRoom:
+		self.failUnless(roomcheck(1))
+
+def main():
+	unittest.main()
+
+if __name__ == '__main__':
+	main()
