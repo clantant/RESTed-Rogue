960511cdbeccf5224ef724a4318332dc4ad5d851
Eric Timmerman <eet1992@gmail.com>
2018-01-13 21:01:21 -0500

    Adding new functions and rolling versions forward

--- restgame/main.py
+++ restgame/main.py
@@ -1,1 +1,14 @@
+# This is the main functionality for the rest game
+# This will include the dungeon object definition and some core utilities
+# Later there may be a point to refactor the utilities into their own files
 
+# Dungeon class, this is our main object and represents all of the rooms and the player
+class Dungeon:
+	def __init__(self, name):
+		self.rooms=[]
+		self.player={"name" : name, "pos" : 1}
+
+# Start - starts the game and initializes the dungeon object
+def start(name):
+	instance = Dungeon(name)
+	return 1
