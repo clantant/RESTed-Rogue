6bfcd6013a1953084562681503722a843c81fdb1
Eric Timmerman <eet1992@gmail.com>
2018-01-13 20:37:22 -0500

    Updated setup to reflect necessary dependencies

--- setup.py
+++ setup.py
@@ -1,8 +1,13 @@
-from distutils.core import setup
+try:
+	from setuptools import setup
+except ImportError:
+	from distutils.core import setup
 setup(name='Restgame',
 	version='0.1',
 	author="Eric Timmerman",
 	author_email = "eet1992@gmail.com",
 	description = ("REST based web game"),
+	url="https://github.com/clantant/RESTed-Rogue",
+	install_requires=['Flask'],
 	packages=['restgame', 'test']
 	)
